8589dd585d7e032496211902862dc5df6224b88d
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-03-24 17:54:15 +0530

    added analytics

--- src/app/api/select/route.ts
+++ src/app/api/select/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
           <title>Analytics of stream</title>
           <meta property="fc:frame" content="vNext"/>
           <meta property="fc:frame:image" content="https://i.imgur.com/yErFr19.jpeg" />
-          <meta property="fc:frame:input:text" content="Enter stream link"/>
+          <meta property="fc:frame:input:text" content="Enter Details"/>
           <meta property="fc:frame:button:1" content="Next" />
           <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/view-analytics"/>
       </head>
--- src/app/api/view-analytics/route.ts
+++ src/app/api/view-analytics/route.ts
@@ -1,20 +1,63 @@
 import { Livepeer } from "livepeer";
-import { GetCreatorMetricsRequest, QueryParamBreakdownBy, QueryParamTimeStep } from "livepeer/dist/models/operations";
+import {
+  GetCreatorMetricsRequest,
+  QueryParamBreakdownBy,
+  QueryParamTimeStep,
+} from "livepeer/dist/models/operations";
 import { NextRequest, NextResponse } from "next/server";
+import pinataSDK from "@pinata/sdk";
+import fs from "fs";
+import path from "path";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const data = await req.json();
   const inputText = data.untrustedData.inputText;
-  console.log("input", inputText);  // @fetch stream key and show below
+  console.log("input", inputText); // @fetch stream key and show
+
+  const bearerToken = inputText.split(", ")[0];
+  //* stream id
+  const id = inputText.split(", ")[1];
 
   const livepeer = new Livepeer({
-    apiKey: process.env.NEXT_PUBLIC_LIVEPEER_KEY,
+    apiKey: bearerToken,
   });
- 
-  const response = await livepeer.metrics.getPublicTotalViews(inputText)
+
+  const response = await livepeer.stream.get(id);
   const str = String.fromCharCode.apply(String, response.rawResponse?.data);
   const obj = JSON.parse(str);
-  console.log("xxx", obj) // @Vaibhav dynamic og image to show on screen
+  console.log("xxx", obj); // @Vaibhav dynamic og image to show on screen
+
+  const pinata = new pinataSDK(
+    process.env.NEXT_PUBLIC_PINATA_API_KEY,
+    process.env.NEXT_PUBLIC_PINATA_API_SECRET
+  );
+
+  const svgContent = `<svg width="800" height="418" xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <rect width="100%" height="100%" fill="black" />      
+      <text x="20" y="100" font-family="Arial" font-size="20" fill="white">name: ${
+        obj.name
+      }</text>
+      <text x="20" y="150" font-family="Arial" font-size="20" fill="white">stream key: ${
+        obj.streamKey
+      }</text>
+      <text x="20" y="200" font-family="Arial" font-size="20" fill="white">playback id: ${
+        obj.playbackId
+      }</text>
+      <text x="20" y="250" font-family="Arial" font-size="20" fill="white">isHealthy: ${
+        obj.isHealthy === undefined ? false : obj.isHealthy
+      }</text>
+      <text x="20" y="300" font-family="Arial" font-size="20" fill="white">isActive: ${
+        obj.isActive
+      }</text>
+      </svg>`;
+
+  const p = path.join(__dirname, "data.svg");
+
+  fs.writeFile(p, svgContent, (err) => {
+    console.log(err);
+  });
+
+  const resp = await pinata.pinFromFS(p);
 
   return new NextResponse(`   
   <!DOCTYPE html>
@@ -22,7 +65,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         <head>
         <title>Analytics of stream</title>
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://i.imgur.com/aC65jWk.jpeg"/>
+          <meta property="fc:frame:image" content="https://beige-emotional-dragon-806.mypinata.cloud/ipfs/${resp.IpfsHash}"/>
           <meta property="fc:frame:button:1" content="Done" />
           <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/home"/>
           </head>
