c6269442ce2fb190aaa4b0f9b6ac0c231172d8e4
Aman-Mandal <002amankumar@gmail.com>
2024-03-24 18:56:14 +0530

    New og images for different sections

--- src/app/api/stream-api-key/route.ts
+++ src/app/api/stream-api-key/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         <head>
         <title>Stream api key</title>
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://i.postimg.cc/k5ZNCYJD/title.png"/>
+          <meta property="fc:frame:image" content="https://i.postimg.cc/J4XhgygV/Vemoir-2.png"/>
           <meta property="fc:frame:button:1" content="Go live" />
           <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/og-image"/>
           </head>
--- src/app/api/stream-live-video/route.ts
+++ src/app/api/stream-live-video/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         <head>
         <title>Watch live stream here</title>
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://i.postimg.cc/JzfWSmfF/started.png"/>
+          <meta property="fc:frame:image" content="https://i.postimg.cc/XJwZTQQ5/Vemoir-4.png"/>
           <meta property="fc:frame:button:1" content="View stream here" />
           <meta property="fc:frame:button:1:action" content="link"/>
           <meta property="fc:frame:button:1:target" content="${process.env.NEXT_PUBLIC_BASE_URL}/start?id=${inputText}"/>
